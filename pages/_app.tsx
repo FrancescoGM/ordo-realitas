cc0b99bc206bc20deaab2398199c266a15bca3c3
FrancescoGM <francescogmuller@hotmail.com>
2022-02-05 10:28:29 -0300

    feat(configuration): eslint and prettier have been configured

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
-import type { AppProps } from "next/app";
+import type { AppProps } from 'next/app'
 
-export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+export default function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+  return <Component {...pageProps} />
 }
