052e7a558c9216c6b7dd0e6a23cac0f37d3f6853
FrancescoGM <francescogmuller@hotmail.com>
2022-02-06 23:36:16 -0300

    fix: meta tags

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -48,8 +48,10 @@ export default class Document extends NextDocument {
             property="og:description"
             content="Ordo Realitas é uma plataforma para os jogadores de ordem paranormal rpg armazenarem seus personagens e ter uma experiência imersiva como na websérie de Ordem Paranormal."
           />
-          <meta property="og:image" content="/thumbnail.png" />
-
+          <meta
+            property="og:image"
+            content="https://ordo-realitas.com.br/thumbnail.png"
+          />
           <meta property="twitter:card" content="summary_large_image" />
           <meta property="twitter:url" content="https://ordo-realitas.com.br" />
           <meta property="twitter:title" content="Ordo Realitas" />
@@ -57,7 +59,13 @@ export default class Document extends NextDocument {
             property="twitter:description"
             content="Ordo Realitas é uma plataforma para os jogadores de ordem paranormal rpg armazenarem seus personagens e ter uma experiência imersiva como na websérie de Ordem Paranormal."
           />
-          <meta property="twitter:image" content="/thumbnail.png" />
+          <meta
+            property="twitter:image"
+            content="https://ordo-realitas.com.br/thumbnail.png"
+          />
+          <meta name="viewport" content="width=device-width" />
+          <meta charSet="utf-8" />
+          <meta name="next-head-count" content="4" />
         </Head>
         <body>
           <Main />
